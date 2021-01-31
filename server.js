b06f97cb2cd2ab4225691b3f23796fe9d0c0bcc0
vivg123 <www.gusain.vivek@gmail.com>
2021-01-31 22:59:58 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  }); 
+  
+
+app.use("/public", express.static("./public"));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+console.log("listening on http://localhost:3000");
